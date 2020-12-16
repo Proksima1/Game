78537651f29f2ea9519e04255c3688a0f8ece174
EveRWhateveR-python <65110318+EveRWhateveR-python@users.noreply.github.com>
2020-12-16 19:21:23 +0300

    Revert "Изменил прогресс бар и остальное"
    This reverts commit 3bbf55a3b123ccdefd47b1fda0862221b7b2cc0d.

--- classes/fregate.py
+++ classes/fregate.py
@@ -1,9 +1,7 @@
-from random import randint
-from threading import Thread
-from time import sleep
-
 import pygame
-
+from random import randint
+from pygame.locals import *
+from time import sleep
 from progressbar import ProgressBar
 
 
@@ -22,8 +20,8 @@ class Generel_ship(pygame.sprite.Sprite):
         self.bar = ProgressBar(self.screen, "red", self.rect.x, self.rect.y + 10, 25, 5)
 
     def update_bar(self):
-        self.bar.x = self.rect.x
-        self.bar.y = self.rect.y
+        self.bar.left = self.rect.x
+        self.bar.top = self.rect.y
         self.bar.draw(self.hp)
 
     def down(self):
@@ -31,28 +29,24 @@ class Generel_ship(pygame.sprite.Sprite):
         if int(self.rect.y) + 59 <= self.screen.get_height():
             self.y += self.velocity
             self.rect.y = self.y
-        self.make_a_ship()
 
     def up(self):
         # перемещение вверх
         if int(self.y) + 5 >= 0:
             self.y -= self.velocity
             self.rect.y = self.y
-        self.make_a_ship()
 
     def left(self):
         # перемещение налево
         if int(self.x) + 2 >= 0:
             self.x -= self.velocity
             self.rect.x = self.x
-        self.make_a_ship()
 
     def right(self):
         # перемещение направо
         if int(self.rect.x) + 64 <= self.screen.get_width():
             self.x += self.velocity
             self.rect.x = self.x
-        self.make_a_ship()
 
     def make_a_ship(self):
         self.screen.blit(self.image, self.rect)
@@ -69,12 +63,7 @@ class Enemy_ship(Generel_ship):
         self.velocity = 0.3
         self.stop = 1
 
-    def update_all_systems(self):
-        self.update_bar()
-        self.make_a_ship()
-
-    def random_move(self, screen):
-        screen.fill((0, 0, 0))
+    def random_move(self):
         while True:
             a = randint(1, 6)
             if a == 1:
@@ -83,7 +72,6 @@ class Enemy_ship(Generel_ship):
                         if self.screen.get_width() // 2 <= self.rect.x:
                             sleep(0.001)
                             self.left()
-                            self.update_all_systems()
                 self.movement = "left"
 
             if a == 2:
@@ -92,7 +80,6 @@ class Enemy_ship(Generel_ship):
                         if self.screen.get_width() - 100 >= self.rect.x:
                             sleep(0.001)
                             self.right()
-                            self.update_all_systems()
                 self.movement = "right"
 
             if a == 3:
@@ -100,17 +87,14 @@ class Enemy_ship(Generel_ship):
                     if 150 <= self.rect.y:
                         sleep(0.001)
                         self.up()
-                        self.update_all_systems()
             if a == 4:
                 for _ in range(randint(80, 100)):
                     if self.screen.get_width() - 150 >= self.rect.y:
                         sleep(0.001)
                         self.down()
-                        self.update_all_systems()
 
             if a == 5 or a == 6:
                 sleep(0.025)
-                self.update_all_systems()
 
 
 class Player_ship(Generel_ship):
@@ -151,10 +135,5 @@ class Enemy_controller:
     def append(self, value: Enemy_ship):
         self.list_of_enemies.append(value)
 
-    def update_all(self, screen):
-        for enemy in self.list_of_enemies:
-            thread = Thread(target=enemy.random_move, args=(screen,))
-            thread.start()
-            # enemy.random_move()
-            sleep(0.001)
-            # creen.fill((0, 0, 0))
+    def update_all(self):
+        pass
